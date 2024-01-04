e16ce50d0e9f42ae47ec17fd759e49f27912eee0
Igor Radovanov <igor@radovanovigor.com>
2024-01-04 20:12:11 +0100

    Add tests

--- arrays/test_static_array.py
+++ arrays/test_static_array.py
@@ -1,4 +1,4 @@
-from static_array import insertEnd, removeEnd, removeMiddle
+from static_array import *
 
 def test_insertEnd():
     # Test case 1: Inserting an element into an empty array
@@ -22,7 +22,7 @@ def test_insertEnd():
     insertEnd(arr, 6, length, capacity)
     assert arr == [1, 2, 3, 4, 5]  # The array should remain unchanged
 
-    print("All test cases passed!")
+    print("All test cases pass")
 
 test_insertEnd()
 
@@ -37,7 +37,7 @@ def test_removeEnd():
     arr = [1, 2, 3, None, None]
     length = 3
     removeEnd(arr, length)
-    assert arr == [1, 2, None, None, None]
+    assert arr == [1, 2, 0, None, None]
 
     # Test case 3: Removing an element from a full array
     arr = [1, 2, 3, 4, 5]
@@ -51,6 +51,39 @@ def test_removeEnd():
     removeEnd(arr, length)
     assert arr == [0]
 
-    print("All test cases passed!")
+    print("All test cases pass")
 
-test_removeEnd()
+
+def test_insertMiddle():
+    # Test case 1: Inserting an element into an empty array
+    arr = [None] * 5
+    length = 0
+    insertMiddle(arr, 0, 10, length)
+    assert arr == [10, None, None, None, None]
+
+    # Test case 2: Inserting an element into the middle of a partially filled array
+    arr = [1, 2, None, None, None]
+    length = 2
+    insertMiddle(arr, 1, 3, length)
+    assert arr == [1, 3, 2, None, None]
+
+    print("All test cases pass")
+
+test_insertMiddle()
+
+def test_removeMiddle():
+    # Test case 1: Removing the middle element from an array with odd length
+    arr = [1, 2, 3, 4, 5]
+    length = 5
+    removeMiddle(arr, 2, length)
+    assert arr == [1, 2, 4, 5, 5]
+
+    # Test case 2: Removing the middle element from an array with even length
+    arr = [1, 2, 3, 4]
+    length = 4
+    removeMiddle(arr, 1, length)
+    assert arr == [1, 3, 4, 4]
+
+    print("All test cases pass")
+
+test_removeMiddle()
