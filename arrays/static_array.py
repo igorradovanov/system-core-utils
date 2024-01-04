e5903475529045f689dd14494bcede32bc54bfd2
Igor Radovanov <igor@radovanovigor.com>
2024-01-04 19:57:55 +0100

    Define insertMiddle()

--- arrays/static_array.py
+++ arrays/static_array.py
@@ -29,6 +29,23 @@ def removeEnd(arr, length):
         # Overwrite value and decrease the length
         arr[length - 1] = 0
 
+def insertMiddle(arr, i, n, length):
+    """
+    Inserts an element at the middle index of the array and shifts the remaining elements to the right.
+
+    Args:
+        arr (list): The input array.
+        i (int): The index at which the element needs to be inserted.
+        n: The element to be inserted.
+        length (int): The length of the array.
+
+    Returns:
+        None
+    """
+    for index in range(length - 1, i - 1, -1):
+        arr[index + 1] = arr[index]
+    arr[i] = n
+
 def removeMiddle(arr, i, length):
     """
     Removes the element at the middle index of the array and shifts the remaining elements to the left.
