0aa10da675a826562e64529aac616b06403076b2
Igor Radovanov <igor@radovanovigor.com>
2024-01-04 19:48:19 +0100

    Implement removeEnd()

--- arrays/static_array.py
+++ arrays/static_array.py
@@ -13,3 +13,22 @@ def insertEnd(arr, n, length, capacity):
     """
     if length < capacity:
         arr[length] = n
+
+def removeEnd(arr, length):
+    """
+    Removes the element at the end of the array and updates the length.
+
+    Args:
+        arr (list): The array from which the element needs to be removed.
+        length (int): The current length of the array.
+
+    Returns:
+        None
+    """
+    if length > 0:
+        # Overwrite value and decrease the length
+        arr[length - 1] = 0
+def removeEnd(arr, length):
+    if length > 0:
+        #Overwrite value and decrease the length
+        arr[length - 1] = 0
