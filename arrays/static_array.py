33adba380892898da0bdc9c9028e198959acc8e4
Igor Radovanov <igor@radovanovigor.com>
2024-01-06 14:23:35 +0100

    Refactor static_array.py and test_static_array.py

--- arrays/static_array.py
+++ arrays/static_array.py
@@ -1,101 +1,112 @@
-from ast import List
+from typing import List
 
+class StaticArray:
+    def insertEnd(self, arr, n, length, capacity):
+        """
+        Inserts an element at the end of the array.
 
-def insertEnd(arr, n, length, capacity):
-    """
-    Inserts an element at the end of the array.
+        Args:
+            arr (list): The array to insert the element into.
+            n: The element to be inserted.
+            length (int): The current length of the array.
+            capacity (int): The maximum capacity of the array.
 
-    Args:
-        arr (list): The array to insert the element into.
-        n: The element to be inserted.
-        length (int): The current length of the array.
-        capacity (int): The maximum capacity of the array.
+        Returns:
+            None
+        """
+        if length < capacity:
+            arr[length] = n
 
-    Returns:
-        None
-    """
-    if length < capacity:
-        arr[length] = n
+    def removeEnd(self, arr, length):
+        """
+        Removes the element at the end of the array and updates the length.
 
-def removeEnd(arr, length):
-    """
-    Removes the element at the end of the array and updates the length.
+        Args:
+            arr (list): The array from which the element needs to be removed.
+            length (int): The current length of the array.
 
-    Args:
-        arr (list): The array from which the element needs to be removed.
-        length (int): The current length of the array.
+        Returns:
+            None
+        """
+        if length > 0:
+            # Overwrite value and decrease the length
+            arr[length - 1] = 0
 
-    Returns:
-        None
-    """
-    if length > 0:
-        # Overwrite value and decrease the length
-        arr[length - 1] = 0
+    def insertMiddle(self, arr, i, n, length):
+        """
+        Inserts an element at the middle index of the array and shifts the remaining elements to the right.
 
-def insertMiddle(arr, i, n, length):
-    """
-    Inserts an element at the middle index of the array and shifts the remaining elements to the right.
+        Args:
+            arr (list): The input array.
+            i (int): The index at which the element needs to be inserted.
+            n: The element to be inserted.
+            length (int): The length of the array.
 
-    Args:
-        arr (list): The input array.
-        i (int): The index at which the element needs to be inserted.
-        n: The element to be inserted.
-        length (int): The length of the array.
+        Returns:
+            None
+        """
+        for index in range(length - 1, i - 1, -1):
+            arr[index + 1] = arr[index]
+        arr[i] = n
 
-    Returns:
-        None
-    """
-    for index in range(length - 1, i - 1, -1):
-        arr[index + 1] = arr[index]
-    arr[i] = n
+    def removeMiddle(self, arr, i, length):
+        """
+        Removes the element at the middle index of the array and shifts the remaining elements to the left.
 
-def removeMiddle(arr, i, length):
-    """
-    Removes the element at the middle index of the array and shifts the remaining elements to the left.
+        Args:
+            arr (list): The input array.
+            i (int): The index of the middle element to be removed.
+            length (int): The length of the array.
 
-    Args:
-        arr (list): The input array.
-        i (int): The index of the middle element to be removed.
-        length (int): The length of the array.
+        Returns:
+            None
+        """
+        for index in range(i + 1, length):
+            arr[index - 1] = arr[index]
+            
 
-    Returns:
-        None
-    """
-    for index in range(i + 1, length):
-        arr[index - 1] = arr[index]
+    def removeDuplicates(self, nums: List[int]) -> int:
+        """
+        Removes duplicates from the given list of integers.
+
+        Args:
+            nums (List[int]): The list of integers.
+
+        Returns:
+            int: The length of the modified list without duplicates.
+        """
+        l = 1
+        for r in range(1, len(nums)):
+            if(nums[r] != nums[r-1]):
+                nums[l] = nums[r]
+                l = l + 1
+        return l
+
+    def removeElement(self, nums: List[int], val: int) -> int:
+        """
+        Removes all occurrences of a specified value from the given list.
+
+        Args:
+            nums (List[int]): The list of integers.
+            val (int): The value to be removed.
+
+        Returns:
+            int: The new length of the list after removing the specified value.
+        """
+        i, j = 0, 0
+        for j in range(len(nums)):
+            if nums[j] != val:
+                nums[i] = nums[j]
+                i += 1
+        return i
+
+    def shuffle(self, nums: List[int], n: int) -> List[int]:
+        arr = []
+        length = len(nums) // 2
+        left = nums[:length]
+        right = nums[length:]
         
-
-def removeDuplicates(self, nums: List[int]) -> int:
-    """
-    Removes duplicates from the given list of integers.
-
-    Args:
-        nums (List[int]): The list of integers.
-
-    Returns:
-        int: The length of the modified list without duplicates.
-    """
-    l = 1
-    for r in range(1, len(nums)):
-        if(nums[r] != nums[r-1]):
-            nums[l] = nums[r]
-            l = l + 1
-    return l
-
-def removeElement(self, nums: List[int], val: int) -> int:
-    """
-    Removes all occurrences of a specified value from the given list.
-
-    Args:
-        nums (List[int]): The list of integers.
-        val (int): The value to be removed.
-
-    Returns:
-        int: The new length of the list after removing the specified value.
-    """
-    i, j = 0, 0
-    for j in range(len(nums)):
-        if nums[j] != val:
-            nums[i] = nums[j]
-            i += 1
-    return i
+        for x in range(len(left)):
+            arr.append(left[x])
+            arr.append(right[x])
+        return arr
--- arrays/test_static_array.py
+++ arrays/test_static_array.py
@@ -1,25 +1,27 @@
-from static_array import *
+from static_array import StaticArray
+
+s_array = StaticArray()
 
 def test_insertEnd():
     # Test case 1: Inserting an element into an empty array
     arr = [None] * 5
     length = 0
     capacity = 5
-    insertEnd(arr, 10, length, capacity)
+    s_array.insertEnd(arr, 10, length, capacity)
     assert arr == [10, None, None, None, None]
 
     # Test case 2: Inserting an element into a partially filled array
     arr = [1, 2, 3, None, None]
     length = 3
     capacity = 5
-    insertEnd(arr, 4, length, capacity)
+    s_array.insertEnd(arr, 4, length, capacity)
     assert arr == [1, 2, 3, 4, None]
 
     # Test case 3: Inserting an element into a full array
     arr = [1, 2, 3, 4, 5]
     length = 5
     capacity = 5
-    insertEnd(arr, 6, length, capacity)
+    s_array.insertEnd(arr, 6, length, capacity)
     assert arr == [1, 2, 3, 4, 5]  # The array should remain unchanged
 
     print("All test cases pass")
@@ -30,25 +32,25 @@ def test_removeEnd():
     # Test case 1: Removing an element from an empty array
     arr = [None] * 5
     length = 0
-    removeEnd(arr, length)
+    s_array.removeEnd(arr, length)
     assert arr == [None, None, None, None, None]
 
     # Test case 2: Removing an element from a partially filled array
     arr = [1, 2, 3, None, None]
     length = 3
-    removeEnd(arr, length)
+    s_array.removeEnd(arr, length)
     assert arr == [1, 2, 0, None, None]
 
     # Test case 3: Removing an element from a full array
     arr = [1, 2, 3, 4, 5]
     length = 5
-    removeEnd(arr, length)
+    s_array.removeEnd(arr, length)
     assert arr == [1, 2, 3, 4, 0]
 
     # Test case 4: Removing an element from an array with length 1
     arr = [10]
     length = 1
-    removeEnd(arr, length)
+    s_array.removeEnd(arr, length)
     assert arr == [0]
 
     print("All test cases pass")
@@ -58,13 +60,13 @@ def test_insertMiddle():
     # Test case 1: Inserting an element into an empty array
     arr = [None] * 5
     length = 0
-    insertMiddle(arr, 0, 10, length)
+    s_array.insertMiddle(arr, 0, 10, length)
     assert arr == [10, None, None, None, None]
 
     # Test case 2: Inserting an element into the middle of a partially filled array
     arr = [1, 2, None, None, None]
     length = 2
-    insertMiddle(arr, 1, 3, length)
+    s_array.insertMiddle(arr, 1, 3, length)
     assert arr == [1, 3, 2, None, None]
 
     print("All test cases pass")
@@ -75,13 +77,13 @@ def test_removeMiddle():
     # Test case 1: Removing the middle element from an array with odd length
     arr = [1, 2, 3, 4, 5]
     length = 5
-    removeMiddle(arr, 2, length)
+    s_array.removeMiddle(arr, 2, length)
     assert arr == [1, 2, 4, 5, 5]
 
     # Test case 2: Removing the middle element from an array with even length
     arr = [1, 2, 3, 4]
     length = 4
-    removeMiddle(arr, 1, length)
+    s_array.removeMiddle(arr, 1, length)
     assert arr == [1, 3, 4, 4]
 
     print("All test cases pass")
